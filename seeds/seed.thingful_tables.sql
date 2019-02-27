b2d2ddc540cb4d1ea35671c1b6335729991f2f13
Joshua Reade Harris <joshua.reade.harris@gmail.com>
2019-02-26 22:29:42 -0800

    Update seeds.

--- seeds/seed.thingful_tables.sql
+++ seeds/seed.thingful_tables.sql
@@ -17,16 +17,16 @@ VALUES
 
 INSERT INTO thingful_things (title, image, user_id, content)
 VALUES
-  ('Hand-Painted Rubber Ducky', 'https://loremflickr.com/250/250/landscape?random=1', 1, 'This ducky has been hand-painted and is now art. Therefore it is useless and cannot be put in water.'),
-  ('Cloning Machine', 'https://loremflickr.com/250/250/landscape?random=2', 2, '100% guaranteed to occasionally work every time! Requires a 167.23v power outlet or a dragonscale battery (obtained separately by solving a riddle).'),
-  ('Kangaroo Carrier', 'https://loremflickr.com/250/250/landscape?random=3', 3, 'This convenient item can assist you in bringing your kangaroo to your favorite grocery store, or as a conversation starter at a first date or funeral.'),
-  ('Love Potion #26', 'https://loremflickr.com/250/250/landscape?random=4', 4, 'While not as well known as its predecessor, Love Potion #9, this formulation has been proven to be effective in winning the affections of some small amphibians.'),
-  ('My Freeze Ray', 'https://loremflickr.com/250/250/landscape?random=5', 5, 'With this freeze ray, you can stop the world.'),
-  ('iPhone EMP Generator', 'https://loremflickr.com/250/250/landscape?random=6', 6, 'With its guaranteed 1m radius, this discreet device will disable an entire busload of iPhones with the push of a button. It is recommended to include an analog camera which can record the entertaining looks on the faces of those affected, as well as a riot shield in case of mass hysteria.'),
-  ('Foolproof Instant Wealth Pamphlet', 'https://loremflickr.com/250/250/landscape?random=7', 1, 'Purchase this pamphlet for $100. Sell this pamphlet to a billion people for $100. Acquisition of this pamphlet is indeed proof of foolishness!'),
-  ('Story Water Spigot', 'https://loremflickr.com/250/250/landscape?random=8', 2, 'Once installed by a qualified leprechaun, this spigot will produce a steady stream of stories which can be later be adapted to motion pictures which will not be quite as good as the originals.'),
-  ('Little Red Slippers', 'https://loremflickr.com/250/250/landscape?random=9', 3, 'Get home quicker than either Uber or Lyft! Three taps of the heels is all it takes. One size fits all.'),
-  ( 'Magic Lamp', 'https://loremflickr.com/250/250/landscape?random=10', 4, 'May or may not produce a genie.');
+  ('Hand-Painted Rubber Ducky', 'https://loremflickr.com/750/300/landscape?random=1', 1, 'This ducky has been hand-painted and is now art. Therefore it is useless and cannot be put in water.'),
+  ('Cloning Machine', 'https://loremflickr.com/750/300/landscape?random=2', 2, '100% guaranteed to occasionally work every time! Requires a 167.23v power outlet or a dragonscale battery (obtained separately by solving a riddle).'),
+  ('Kangaroo Carrier', 'https://loremflickr.com/750/300/landscape?random=3', 3, 'This convenient item can assist you in bringing your kangaroo to your favorite grocery store, or as a conversation starter at a first date or funeral.'),
+  ('Love Potion #26', 'https://loremflickr.com/750/300/landscape?random=4', 4, 'While not as well known as its predecessor, Love Potion #9, this formulation has been proven to be effective in winning the affections of some small amphibians.'),
+  ('My Freeze Ray', 'https://loremflickr.com/750/300/landscape?random=5', 5, 'With this freeze ray, you can stop the world.'),
+  ('Personal EMP Generator', 'https://loremflickr.com/750/300/landscape?random=6', 6, 'With its guaranteed 10m radius, this discreet device will disable an entire busload of iPhones with the push of a button. It is recommended to include an analog camera which can record the entertaining looks on the faces of those affected, as well as a riot shield in case of mass hysteria.'),
+  ('Foolproof Instant Wealth Pamphlet', 'https://loremflickr.com/750/300/landscape?random=7', 1, 'Purchase this pamphlet for $100. Sell this pamphlet to a billion people for $100. Acquisition of this pamphlet is indeed proof of foolishness!'),
+  ('Story Water Spigot', 'https://loremflickr.com/750/300/landscape?random=8', 2, 'Once installed by a qualified leprechaun, this spigot will produce a steady stream of stories which can be later be adapted to motion pictures which will not be quite as good as the originals.'),
+  ('Ruby Red Slippers', 'https://loremflickr.com/750/300/landscape?random=9', 3, 'Get home quicker than either Uber or Lyft! Three taps of the heels is all it takes. One size fits all.'),
+  ( 'Magic Lamp', 'https://loremflickr.com/750/300/landscape?random=10', 4, 'May or may not produce a genie.');
 
 INSERT INTO thingful_reviews (
   text,
@@ -71,7 +71,7 @@ INSERT INTO thingful_reviews (
     1
   ),
   (
-    'I have not used it, or even seen it, or actually know what it is. I do not know why I am even writing this review, or how I got here, or what my name is. Three stars!',
+    'I have not used it or even seen it, and I do not actually know what it is. I do not know why I am writing this review, how I got here, or what my name is. Three stars!',
     3,
     2,
     3
@@ -101,13 +101,13 @@ INSERT INTO thingful_reviews (
     5
   ),
   (
-    'There are some better things, but there are also some worse things.',
+    'There are some better things. There are also some worse things.',
     3,
     7,
     1
   ),
   (
-    'Great holiday present for extraterrestrials, but only the kind with the lightbulb heads.',
+    'Great holiday present for extraterrestrials (only the kind with the lightbulb heads).',
     4,
     7,
     2
